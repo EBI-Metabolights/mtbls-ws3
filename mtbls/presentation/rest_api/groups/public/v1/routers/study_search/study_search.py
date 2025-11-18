69b0b5749e457f13555b061846e087830bafc3f4
cmartin <cmartin@ebi.ac.uk>
2025-11-18 15:37:50 +0000

    tidied up search endpoint

--- mtbls/presentation/rest_api/groups/public/v1/routers/study_search/study_search.py
+++ mtbls/presentation/rest_api/groups/public/v1/routers/study_search/study_search.py
@@ -2,19 +2,11 @@ from logging import getLogger
 from typing import Annotated
 
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends, Path, Response, status, Body
+from fastapi import APIRouter, Depends, Response, status, Body
 
 from mtbls.application.services.interfaces.http_client import HttpClient
-from mtbls.application.services.interfaces.repositories.statistic.statistic_read_repository import (  # noqa E501
-    StatisticReadRepository,
-)
 from mtbls.domain.entities.search.study.index_search import IndexSearchInput, IndexSearchResult
 from mtbls.presentation.rest_api.core.responses import APIErrorResponse, APIResponse
-from mtbls.presentation.rest_api.groups.public.v1.routers.statistics.schemas import (
-    MetricData,
-    StatisticCategory,
-    StatisticData,
-)
 
 logger = getLogger(__name__)
 
@@ -31,9 +23,6 @@ router = APIRouter(tags=["Public"], prefix="/public/v2/public-study-index")
 async def search_study_index(
     response: Response,
     q: Annotated[IndexSearchInput, Body()],
-    http_client: HttpClient = Depends(
-        Provide["gateways.http_client"]
-    ),
     elasticsearch_study_search_service = Depends(
         Provide["gateways.elasticsearch_study_gateway"]
     ),
