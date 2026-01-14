8cfdaa865a69d30565cb5c9cb38259cd69b612b1
oyurekten <ozgury@ebi.ac.uk>
2026-01-14 21:52:57 +0000

    Study creation configuration update

--- mtbls/domain/entities/validation/validation_configuration.py
+++ mtbls/domain/entities/validation/validation_configuration.py
@@ -436,6 +436,7 @@ class OntologySourceReferenceTemplate(StudyBaseModel):
     source_description: Annotated[
         str, Field(description="Source description and full name")
     ]
+    source_details: Annotated[str, Field(description="Source details")] = ""
 
 
 class DefaultControl(StudyBaseModel):
@@ -447,6 +448,19 @@ class ActiveMhdProfile(StudyBaseModel):
     profile_name: Annotated[str, Field(description="profile name")]
     default_version: Annotated[str, Field(description="default profile version")]
     active_versions: Annotated[list[str], Field(description="active profile versions")]
+
+
+class ActiveStudyCategory(StudyBaseModel):
+    visible: Annotated[bool, Field(description="Visible")] = True
+    order: Annotated[int, Field(description="Category order")]
+
+
+class ActiveDesignDescriptorCategory(StudyBaseModel):
+    visible: Annotated[bool, Field(description="Visible")] = True
+    order: Annotated[int, Field(description="Category order")]
+    min: Annotated[
+        int, Field(description="Number of minimum required design descriptors")
+    ]
 
 
 class TemplateConfiguration(StudyBaseModel):
@@ -463,7 +477,7 @@ class TemplateConfiguration(StudyBaseModel):
         list[str], Field(description="active assay file templates")
     ]
     active_study_categories: Annotated[
-        list[str], Field(description="active study categories")
+        dict[str, ActiveStudyCategory], Field(description="active study categories")
     ]
     active_dataset_licenses: Annotated[
         list[str], Field(description="active dataset licenses")
@@ -473,11 +487,11 @@ class TemplateConfiguration(StudyBaseModel):
         Field(description="active dataset licenses"),
     ]
     active_study_design_descriptor_categories: Annotated[
-        list[str],
+        dict[str, ActiveDesignDescriptorCategory],
         Field(description="active study design descriptor categories"),
     ]
     active_assay_design_descriptor_categories: Annotated[
-        list[str],
+        dict[str, ActiveDesignDescriptorCategory],
         Field(description="active assay design descriptor categories"),
     ]
     default_sample_file_template: Annotated[
@@ -586,6 +600,7 @@ class DefaultCommentConfiguration(StudyBaseModel):
 class DescriptorCategoryDefinition(StudyBaseModel):
     name: Annotated[str, Field(description="study category name")]
     label: Annotated[str, Field(description="study category label")]
+    isa_file_type: Annotated[MetadataFileType, Field(description="ISA-TAB file type.")]
     control_list_key: Annotated[
         None | str, Field(description="study category description")
     ]
@@ -638,7 +653,8 @@ class TemplateSettings(StudyBaseModel):
         DefaultCommentConfiguration, Field(description="default comment configuration")
     ]
     study_categories: Annotated[
-        dict[str, StudyCategoryDefinition], Field(description="study categories")
+        dict[StudyCategoryStr, StudyCategoryDefinition],
+        Field(description="study categories"),
     ] = {}
     mhd_profiles: Annotated[
         dict[str, dict[str, MhdProfileInfo]],
