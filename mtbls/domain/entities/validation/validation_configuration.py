a99a7e59d6a5d213b7bbf94a0e231988d6109413
oyurekten <ozgury@ebi.ac.uk>
2026-01-16 08:23:22 +0000

    assay creation related updates

--- mtbls/domain/entities/validation/validation_configuration.py
+++ mtbls/domain/entities/validation/validation_configuration.py
@@ -469,6 +469,9 @@ class AssayFileConfiguration(StudyBaseModel):
     description: Annotated[
         str, Field(description="description of the predefined value")
     ]
+    ontology_term: Annotated[
+        OntologyTerm, Field(description="ontology term for the assay")
+    ]
     assay_file_default_values: Annotated[
         dict[str, list[str]],
         Field(description="assay groups (protocols, file, etc.) and parameters"),
