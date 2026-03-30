31420789922b0216b4ee6997cb6de06bf4bc66b3
oyurekten <ozgury@ebi.ac.uk>
2026-03-30 06:39:31 +0000

    mcp server path update

--- mtbls/run/mcp/submission/main.py
+++ mtbls/run/mcp/submission/main.py
@@ -23,21 +23,20 @@ async def start_mcp_server(config_file_path: str, secrets_file_path: str):
     fast_app: FastAPI = await get_app(
         config_file_path, secrets_file_path, app_container
     )
-    fast_app.add_api_route(
-        path="/health",
-        endpoint=health_check,
-        methods=["GET"],
-        response_class=JSONResponse,
-        tags=["Health Check"],
-    )
     mcp = FastMCP.from_fastapi(app=fast_app)
     server_configuration: ApiServerConfiguration = app_container.api_server_config()
     config = server_configuration.server_info
+    root_path = config.root_path.rstrip("/") if config.root_path else ""
+    if root_path:
+        parts = root_path.split("/")
+        if len(parts) > 0:
+            root_path = "/".join(parts[:-1])
+
     await mcp.run_async(
         transport="http",
         host="0.0.0.0",
         port=int(server_configuration.port),
-        path=f"{config.root_path}/mcp/",
+        path=f"{root_path}/mcp/",
     )
 
 
